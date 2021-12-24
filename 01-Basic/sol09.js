98ee536e23a630efd9299bf1f75199f0c948ac9b
Eunyeol <ney9083@gmail.com>
2021-12-24 23:09:54 +0900

    20211224 A를 #으로

---
+++ 01-Basic/sol09.js
@@ -0,0 +1,17 @@
+function solution(word) {
+  let answer = "";
+  // for 문을 통해 문자를 하나씩 탐색
+  for (let i of word) {
+    // 문자열이 "A"인 경우 "#"으로 변환하여 출력문자에 추가
+    if (i === "A") {
+      answer += "#";
+      // "A"가 아닐 경우 그대로 출력문자에 추가
+    } else answer += i;
+  }
+
+  return answer;
+}
+
+let word = "BANANA";
+
+console.log(solution(word));
