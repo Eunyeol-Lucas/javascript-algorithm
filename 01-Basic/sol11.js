3c0612b834017df5f7152a52784407ff9757e545
Eunyeol <ney9083@gmail.com>
2021-12-24 23:17:45 +0900

    20211224 대문자 찾기

---
+++ 01-Basic/sol11.js
@@ -0,0 +1,16 @@
+function solution(str) {
+  let answer = 0;
+  // for 문을 통해 문자열을 탐색
+  for (let x of str) {
+    // 문자열이 toUpperCase 함수를 통해 대문자로 변환한 문자열과 일치할 경우
+    if (x === x.toUpperCase()) {
+      // 출력변수를 1씩 추가
+      answer++;
+    }
+  }
+
+  return answer;
+}
+
+let str = "KoreaTimeGoodD";
+console.log(solution(str));
