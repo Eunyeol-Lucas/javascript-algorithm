c2cbb1f791c7cdf7f1b8f8bb25a2fafdbb08644e
Eunyeol <ney9083@gmail.com>
2021-12-24 23:21:19 +0900

    20211224 대문자로 통일

---
+++ 01-Basic/sol12.js
@@ -0,0 +1,11 @@
+function solution(str) {
+  let answer = str;
+  // toUpperCase() 메서드를 통해 모든 문자열을 대문자로 변환
+  answer = answer.toUpperCase();
+
+  return answer;
+}
+
+let str = "ItisTimeToStudy";
+
+console.log(solution(str));
