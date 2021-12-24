730f5a4b86c94c10f80f0d7a8d4638cf3bcc5394
Eunyeol <ney9083@gmail.com>
2021-12-24 23:33:02 +0900

    20211224 대소문자로 변환

---
+++ 01-Basic/sol13.js
@@ -0,0 +1,16 @@
+function solution(str) {
+  let answer = "";
+  // for 문을 통해 문자열을 탐색
+  for (let x of str) {
+    // 문자열이 대문자일 경우 출력 변수에 toLowerCase() 메소드를 통해 소문자를 추가
+    if (x === x.toUpperCase()) answer += x.toLowerCase();
+    // 소문자일 경우 toUpperCase() 메소드로 대문자로 변환하여 출력 문자에 추가
+    else answer += x.toUpperCase();
+  }
+
+  return answer;
+}
+
+let str = "StuDY";
+
+console.log(solution(str));
