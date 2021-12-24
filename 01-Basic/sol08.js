cde2abc825bb5c9dc7826ee37f6c56330cdba3ba
Eunyeol <ney9083@gmail.com>
2021-12-24 23:00:33 +0900

    20211224 일곱 난쟁이

---
+++ 01-Basic/sol08.js
@@ -0,0 +1,23 @@
+function solution(arr) {
+  let answer = arr;
+  // reduce 메소드를 통해 배열의 전체 합을 구함
+  let sum = answer.reduce((acc, cur) => acc + cur);
+  let len = arr.length;
+
+  // 이중 for문을 통해 두 난쟁이를 뺐을 때 합이 100이 되는 경우를 확인
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = i + 1; j < len; j++) {
+      if (sum - (arr[i] + arr[j]) === 100) {
+        // 난쟁이 2명을 찾으면, splice 메소드를 통해 배열에서 난쟁이를 제거
+        answer.splice(j, 1);
+        answer.splice(i, 1);
+      }
+    }
+  }
+
+  return answer;
+}
+
+let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
+
+console.log(solution(arr));
