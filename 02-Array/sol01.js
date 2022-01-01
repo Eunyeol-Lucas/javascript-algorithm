21c81e1ee213a0a0707b4089a42343c3ae611241
Eunyeol <ney9083@gmail.com>
2022-01-01 22:43:55 +0900

    20220101 큰 수 출력하기

---
+++ 02-Array/sol01.js
@@ -0,0 +1,19 @@
+function solution(arr) {
+  // 출력될 배열을 담을 새 빈 배열을 생성
+  let answer = [];
+  // 입력된 배열의 첫 요소를 출력될 배열에 추가
+  answer.push(arr[0]);
+  // for문을 통해 배열의 인덱스 1부터 탐색
+  for (let i = 1; i < arr.length; i++) {
+    // if 문을 통해 배열의 앞 수 보다 클 경우 출력될 배열에 추가
+    if (arr[i] > arr[i - 1]) {
+      answer.push(arr[i]);
+    }
+  }
+
+  return answer;
+}
+
+let arr = [7, 3, 9, 5, 6, 12];
+
+console.log(solution(arr));
