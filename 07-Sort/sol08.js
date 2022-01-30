3655ce099da81d39f09f526b2b00d58c352b4d43
Eunyeol <ney9083@gmail.com>
2022-01-30 22:19:17 +0900

    20220130 회의실 배정

--- 07-Sort/sol08.js
+++ 07-Sort/sol08.js
@@ -1,7 +1,11 @@
 function solution(arr) {
   let answer = 0;
   // 끝나는 시간을 기준으로 정렬
-  arr.sort((a, b) => a[1] - b[1]);
+  arr.sort((a, b) => {
+    // 끝나는 시간이 같을 경우에는 시작 시간을 기준으로 정렬
+    if (a[1] === b[1]) return a[0] - b[0];
+    else return a[1] - b[1];
+  });
   // 회의가 끝나는 시간을 저장할 변수
   let endTime = 0;
   // 회의 시간을 담은 배열을 순회하며
@@ -25,7 +29,7 @@ let arr = [
 ];
 let arr1 = [
   [3, 3],
-  [1, 2],
+  [1, 3],
   [2, 3],
 ];
-console.log(solution(arr));
+console.log(solution(arr1));
