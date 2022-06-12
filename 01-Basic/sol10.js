7726eaaf9b1bab089efd84a1fb3be5e1fb514160
Eunyeol <ney9083@gmail.com>
2022-06-13 00:28:20 +0900

    Solve: 220613 문자찾기

--- 01-Basic/sol10.js
+++ 01-Basic/sol10.js
@@ -1,15 +1,25 @@
-function solution(str, alp) {
-  let answer = 0;
-  for (let x of str) {
-    if (x === alp) {
-      answer++;
+function solution1(str, alp) {
+    let answer = 0;
+    for (let x of str) {
+        if (x === alp) {
+            answer++;
+        }
     }
-  }
 
-  return answer;
+    return answer;
+}
+
+function solution2(str, alp) {
+    let answer = 0;
+    const re = new RegExp(alp, "g");
+    let a = (str.match(re) || []).length;
+    return a;
+
+    return answer;
 }
 
 let str = "COMPUTERPROGRAMMING",
-  alp = "R";
+    alp = "R";
 
-console.log(solution(str, alp));
+console.log(solution1(str, alp));
+console.log(solution2(str, alp));
