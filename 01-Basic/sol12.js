0fa79967372854828110952f307296ef90c5f4e6
Eunyeol <ney9083@gmail.com>
2022-06-13 00:32:19 +0900

    Solve: 220613 대문자로 통일

--- 01-Basic/sol12.js
+++ 01-Basic/sol12.js
@@ -1,11 +1,17 @@
-function solution(str) {
-  let answer = str;
-  // toUpperCase() 메서드를 통해 모든 문자열을 대문자로 변환
-  answer = answer.toUpperCase();
+function solution1(str) {
+    let answer = str;
+    // toUpperCase() 메서드를 통해 모든 문자열을 대문자로 변환
+    answer = answer.toUpperCase();
 
-  return answer;
+    return answer;
+}
+
+function solution2(str) {
+    let answer = str.toUpperCase();
+    return answer;
 }
 
 let str = "ItisTimeToStudy";
 
-console.log(solution(str));
+console.log(solution1(str));
+console.log(solution2(str));
