3387a324f14ef141a10f24a35cdcdc7e91d46eff
Eunyeol <ney9083@gmail.com>
2022-06-13 00:42:16 +0900

    Solve: 220613 가장 긴 문자열

--- 01-Basic/sol14.js
+++ 01-Basic/sol14.js
@@ -1,15 +1,25 @@
-function solution(arr) {
-  let answer = "";
-  // for 문을 통해 배열을 탐색
-  for (let x of arr) {
-    // 해당 문자의 길이가 출력 변수 뮨저 길이보다 클 경우 출력변수를 변경
-    if (x.length > answer.length) answer = x;
-  }
+function solution1(arr) {
+    let answer = "";
+    // for 문을 통해 배열을 탐색
+    for (let x of arr) {
+        // 해당 문자의 길이가 출력 변수 뮨저 길이보다 클 경우 출력변수를 변경
+        if (x.length > answer.length) answer = x;
+    }
 
-  return answer;
+    return answer;
+}
+
+function solution2(arr) {
+    let answer = "";
+    for (let x of arr) {
+        if (x.length > answer.length) answer = x;
+    }
+
+    return answer;
 }
 
 let n = 5,
-  arr = ["teacher", "time", "student", "beautiful", "good"];
+    arr = ["teacher", "time", "student", "beautiful", "good"];
 
-console.log(solution(arr));
+console.log(solution1(arr));
+console.log(solution2(arr));
