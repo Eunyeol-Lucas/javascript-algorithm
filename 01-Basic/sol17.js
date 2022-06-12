fab0b54b4d434304048553615b0493952ee09c19
Eunyeol <ney9083@gmail.com>
2022-06-13 00:55:22 +0900

    Solve: 220613 중복 단어 제거

--- 01-Basic/sol17.js
+++ 01-Basic/sol17.js
@@ -1,14 +1,25 @@
-function solution(arr) {
-  let answer = [];
-  for (let x of arr) {
-    if (!answer.includes(x)) {
-      answer.push(x);
+function solution1(arr) {
+    let answer = [];
+    for (let x of arr) {
+        if (!answer.includes(x)) {
+            answer.push(x);
+        }
     }
-  }
-  return answer;
+    return answer;
+}
+
+function solution2(arr) {
+    const answer = [];
+    for (const x of arr) {
+        if (!answer.includes(x)) {
+            answer.push(x);
+        }
+    }
+    return answer;
 }
 
 let num = 5;
 let arr = ["good", "time", "good", "time", "student"];
 
-console.log(solution(arr));
+console.log(solution1(arr));
+console.log(solution2(arr));
