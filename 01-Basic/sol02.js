dcd6df5eae5b094f1ccec3a396cd8742664ba407
Eunyeol <ney9083@gmail.com>
2022-06-12 23:17:48 +0900

    Solve: 220612 삼각형 판별하기

--- 01-Basic/sol02.js
+++ 01-Basic/sol02.js
@@ -1,12 +1,24 @@
-function solution(a, b, c) {
-  let answer = "YES",
-    max;
-  let total = a + b + c;
-  if (a > b) max = a;
-  else max = b;
-  if (c > max) max = c;
-  if (total - max <= max) answer = "NO";
+function solution1(a, b, c) {
+    let answer = "YES",
+        max;
+    let total = a + b + c;
+    if (a > b) max = a;
+    else max = b;
+    if (c > max) max = c;
+    if (total - max <= max) answer = "NO";
 
-  return answer;
+    return answer;
 }
 
+function solution2(a, b, c) {
+    let answer = "YES",
+        max;
+
+    const total = a + b + c;
+    max = Math.max(a, b, c);
+    if (total - max <= max) answer = "NO";
+    return answer;
+}
+
+console.log(solution1(3, 33, 17));
+console.log(solution2(3, 33, 17));
