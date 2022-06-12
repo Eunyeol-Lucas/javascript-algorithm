837ac6b537d5c18b1a1746a343353f97b421580c
Eunyeol <ney9083@gmail.com>
2022-06-13 00:53:57 +0900

    Solve: 220613 중복 문자 제거

--- 01-Basic/sol16.js
+++ 01-Basic/sol16.js
@@ -1,14 +1,25 @@
-function solution(str) {
-  let answer = "";
-  for (let i of str) {
-    if (!answer.includes(i)) {
-      answer += i;
+function solution1(str) {
+    let answer = "";
+    for (let i of str) {
+        if (!answer.includes(i)) {
+            answer += i;
+        }
     }
-  }
 
-  return answer;
+    return answer;
+}
+
+function solution2(str) {
+    let answer = "";
+    for (let i of str) {
+        if (!answer.includes(i)) {
+            answer += i;
+        }
+    }
+    return answer;
 }
 
 let str = "ksekkset";
 
-console.log(solution(str));
+console.log(solution1(str));
+console.log(solution2(str));
