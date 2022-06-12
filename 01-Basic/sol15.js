a9921df29060a339e566b3aeb9b8ffd8f8157672
Eunyeol <ney9083@gmail.com>
2022-06-13 00:50:03 +0900

    Solve: 220613 가운데 문자 출력

--- 01-Basic/sol15.js
+++ 01-Basic/sol15.js
@@ -1,17 +1,30 @@
-function solution(str) {
-  let answer,
-    num = str.length,
-    mid = Math.floor(num / 2);
-  // 문자의 길이가 홀수일 경우
-  if (num % 2 === 1) {
-    // 가운데 문자열을 출력 변수에 입력
-    answer = str[mid];
-    // 짝수일 경우 가운데 두개의 문자를 출력 변수에 입력
-  } else answer = str[mid - 1] + str[mid];
+function solution1(str) {
+    let answer,
+        num = str.length,
+        mid = Math.floor(num / 2);
+    // 문자의 길이가 홀수일 경우
+    if (num % 2 === 1) {
+        // 가운데 문자열을 출력 변수에 입력
+        answer = str[mid];
+        // 짝수일 경우 가운데 두개의 문자를 출력 변수에 입력
+    } else answer = str[mid - 1] + str[mid];
 
-  return answer;
+    return answer;
+}
+
+function solution2(str) {
+    let num = str.length,
+        mid = Math.floor(num / 2);
+
+    switch (str) {
+        case num % 2:
+            return str[mid];
+        default:
+            return str[mid - 1] + str[mid];
+    }
 }
 
 let str = "beautifulGirls";
 
-console.log(solution(str));
+console.log(solution1(str));
+console.log(solution2(str));
