d81debf39ea08f3441237bb686940ebb7b8580c3
Eunyeol <ney9083@gmail.com>
2022-06-12 23:27:15 +0900

    Solve: 220612 연필 개수

--- 01-Basic/sol03.js
+++ 01-Basic/sol03.js
@@ -1,7 +1,15 @@
 // Math 객체의 ceil 메서드를 사용해 다스 수 도출
-function solution(a) {
-  let answer;
-  answer = Math.ceil(a / 12);
-  return answer;
+function solution1(a) {
+    let answer;
+    answer = Math.ceil(a / 12);
+    return answer;
 }
 
+function solution2(a) {
+    let answer;
+    answer = Math.ceil(a / 12);
+    return answer;
+}
+
+console.log(solution1(25));
+console.log(solution2(25));
