7e0a9e81d75eaf3c39b1fc1eba7cc9ace8e3aede
Eunyeol <ney9083@gmail.com>
2022-06-14 22:00:36 +0900

    Solve: 220614 문자열 압축

--- 03-Search/sol04.js
+++ 03-Search/sol04.js
@@ -1,30 +1,52 @@
-function solution(s, t) {
-  // 최소 거리(값)를 담을 빈 배열 result와 t의 인덱스를 담을 빈 빈배열 arr을 선언 및 할당
-  let answer = [];
-  let arr = [];
-  // for문을 통해 입력받은 문자열에 t의 인덱스를 arr 배열에 저장
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === t) {
-      arr.push(i);
+function solution1(s, t) {
+    // 최소 거리(값)를 담을 빈 배열 result와 t의 인덱스를 담을 빈 빈배열 arr을 선언 및 할당
+    let answer = [];
+    let arr = [];
+    // for문을 통해 입력받은 문자열에 t의 인덱스를 arr 배열에 저장
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === t) {
+            arr.push(i);
+        }
     }
-  }
-  // for문을 통해 s 문자열을 탐색하며 s 문자열에서 t 문자까지의 최소 거리를 구함
-  for (let j = 0; j < s.length; j++) {
-    min = Number.MAX_SAFE_INTEGER;
-    for (const x of arr) {
-      result = Math.abs(j - x);
-      // if 문을 통해 최소값을 min에 재할당
-      if (min > result) {
-        min = result;
-      }
+    // for문을 통해 s 문자열을 탐색하며 s 문자열에서 t 문자까지의 최소 거리를 구함
+    for (let j = 0; j < s.length; j++) {
+        min = Number.MAX_SAFE_INTEGER;
+        for (const x of arr) {
+            result = Math.abs(j - x);
+            // if 문을 통해 최소값을 min에 재할당
+            if (min > result) {
+                min = result;
+            }
+        }
+        // 해당 인덱스의 최소값을 answer 변수의 배열에 저장
+        answer[j] = min;
     }
-    // 해당 인덱스의 최소값을 answer 변수의 배열에 저장
-    answer[j] = min;
-  }
 
-  return answer;
+    return answer;
+}
+
+function solution2(s, t) {
+    const answer = [];
+    const arr = [];
+    const n = s.length;
+
+    for (let i = 0; i < n; i++) {
+        if (s[i] === t) {
+            arr.push(i);
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        let min = Number.MAX_SAFE_INTEGER;
+        for (const x of arr) {
+            min = Math.min(min, Math.abs(i - x));
+        }
+        answer.push(min);
+    }
+
+    return answer;
 }
 
 let s = "teachermode",
-  t = "e";
-console.log(solution(s, t));
+    t = "e";
+console.log(solution1(s, t));
+console.log(solution2(s, t));
--- 03-Search/sol05.js
+++ 03-Search/sol05.js
@@ -1,24 +1,44 @@
-function solution(str) {
-  // 압축된 문자열을 담을 변수 answer를 선언하고, 반복횟수를 확인할 변수 count를 1로 선언
-  let answer = "",
-    count = 1;
-  // for문을 돌며 해당 인덱스의 뒤의 문자와 비교하기 위해 공백 문자를 추가
-  str += " ";
-  // for 문을 통해 str 문자열 탐색
-  for (let i = 0; i < str.length - 1; i++) {
-    // if문을 통해 해당 문자열의 인덱스의 문자와 다음 문자가 같을 경우 count를 1 추가
-    if (str[i] === str[i + 1]) count++;
-    // 문자가 다를 경우, 해당 문자를 answer 변수에 추가하고, count가 1보다 클 경우 answer에 추가
-    else {
-      answer += str[i];
-      if (count > 1) answer += String(count);
-      // count를 1로 초기화
-      count = 1;
+function solution1(str) {
+    // 압축된 문자열을 담을 변수 answer를 선언하고, 반복횟수를 확인할 변수 count를 1로 선언
+    let answer = "",
+        count = 1;
+    // for문을 돌며 해당 인덱스의 뒤의 문자와 비교하기 위해 공백 문자를 추가
+    str += " ";
+    // for 문을 통해 str 문자열 탐색
+    for (let i = 0; i < str.length - 1; i++) {
+        // if문을 통해 해당 문자열의 인덱스의 문자와 다음 문자가 같을 경우 count를 1 추가
+        if (str[i] === str[i + 1]) count++;
+        // 문자가 다를 경우, 해당 문자를 answer 변수에 추가하고, count가 1보다 클 경우 answer에 추가
+        else {
+            answer += str[i];
+            if (count > 1) answer += String(count);
+            // count를 1로 초기화
+            count = 1;
+        }
     }
-  }
-  return answer;
+    return answer;
+}
+
+function solution2(str) {
+    let answer = "";
+    const n = str.length;
+    str += " ";
+    let dup = 0;
+    for (let i = 0; i < n; i++) {
+        if (str[i] === str[i + 1]) {
+            dup++;
+        } else {
+            answer += str[i];
+            if (dup) {
+                answer += String(dup + 1);
+                dup = 0;
+            }
+        }
+    }
+    return answer;
 }
 
 const str = "KKHSSSSSSSE";
 
-console.log(solution(str));
+console.log(solution1(str));
+console.log(solution2(str));
